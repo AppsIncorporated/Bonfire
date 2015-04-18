91cee31556f28b30c48337904be54af98be6f394
AppsIncorporated <john@appsincorporated.com>
2015-04-18 12:03:56 -0400

    Project file

---
+++ Bonfire/EventTypesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Bonfire
+//
+//  Created by John Wallace on 4/18/15.
+//  Copyright (c) 2015 John Wallace. All rights reserved.
+//
+
+import UIKit
+
+class EventTypesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
